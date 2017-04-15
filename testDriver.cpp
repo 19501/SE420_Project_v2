39805f9babc1ee550bbd2735df1ca520e22e7985
19501 <MagnusVictor@Gmail.com>
2017-04-14 23:51:29 -0700

    Modularized Test code
    I put the actual 'testing' within a helper function in the test driver
    .cpp file. main() was then changed to simply loop through a large number
    of randomized test cases, counting the number of passes and failures.

--- testDriver.cpp
+++ testDriver.cpp
@@ -2,13 +2,74 @@
 #include <string>
 #include <iostream>
 
-int test3(int testnum, std::string testString, std::string hashKey, std::string alphabet, std::string transposeKey)
+#define NUM_TESTS 1000000
+
+/////////////////////////////////////////////////////////////////////////////////
+//  These are for generating random strings for testing.
+static const char acceptableChars[] =
+"`1234567890-="
+"~!@#$%^&*()_+"
+"abcdefghijklmnopqrstuvwxyz"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"[]\;',./"
+"{}|:\"<>?"
+"\t ";
+
+int numOptions = sizeof(acceptableChars) - 1;
+
+std::string generateRandomString(int stringLength)
 {
-    std::cout << ""
+    std::string output;
+    for(int counter = 0; counter < (stringLength - 1); counter++)
+        output += acceptableChars[rand() % numOptions];
+    return output;
+}
+
+int test3(int testnum, std::string inputMessage, std::string hashKeyString, std::string encipherAlphabet, std::string transposeKeyString)
+{
+    std::cout << "\n\nTEST #" << testnum << ":";
+
+    std::string encryptedMessage = encipherAllThree(inputMessage, hashKeyString, encipherAlphabet, transposeKeyString);
+    std::string decryptedMessage = decipherAllThree(encryptedMessage, hashKeyString, encipherAlphabet, transposeKeyString);
+
+    //std::cout << "\n\nCiphered: " << encryptedMessage;
+    //std::cout << "\nDecrypted: " << decryptedMessage;
+    //std::cout << "\nOriginal:  " << inputMessage;
+
+    if (decryptedMessage.compare(inputMessage))
+    {
+        std::cout << "\n\n\tSUCCESS\n\n";
+        return 1;
+    }
+    else
+    {
+        std::cout << "\n\n\tFAILURE\n\n";
+        return 0;
+    }
 }
 
 int main()
 {
+    std::cout << "Initializing Test Driver...\n\n";
+    int testsCompleted = 0;
+    int passed = 0, failed = 0;
+    while (testsCompleted < NUM_TESTS)
+    {
+        std::string randomMessage = generateRandomString(1000);
+        std::string randomHashString = generateRandomString(1000);
+        std::string randomAlphabet = generateRandomString(1000);
+        std::string randomTransKey = generateRandomString(1000);
+
+        if (test3(testsCompleted, randomMessage, randomHashString, randomAlphabet, randomTransKey) == 1)
+            passed++;
+        else
+            failed++;
+
+        testsCompleted++;
+    }
+    std::cout << "\n\nTests Passed: " << passed;
+    std::cout << "\nTests Failed: " << failed;
+    /*
     //  TEST CASE #1: SIMPLEST SINGLE STRING INPUT
     std::cout << "TEST CASE #1: SIMPLE EXAMPLE STRING\n\n";
     std::string hashKeyString = "Encrypt";
@@ -32,4 +93,5 @@ int main()
     {
         std::cout << "TEST #1:\tFAILURE\n\n";
     }
+    */
 }
